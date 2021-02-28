7e5ce963f5867e3e895ab29a5a95c2332431a69b
Shubham Dhage <shubham.dhage@swiggy.in>
2021-02-28 21:47:48 +0530

    add get age feature and ui

--- app/src/main/java/com/example/findmyage/MainActivity.kt
+++ app/src/main/java/com/example/findmyage/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.findmyage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buGetAge.setOnClickListener{
+            buttonClick()
+        }
+    }
+
+    fun buttonClick() {
+        // Value of the InputBox
+        val userDOB = Integer.parseInt(etDOB.text.toString())
+
+        // The getInstance() method of java.util.Calendar class is a Static method.
+        // This method is used with calendar object to get the instance of calendar according to current time zone
+        val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+
+        val userAgeInYears = currentYear - userDOB
+
+        // tvShowAge is the Id of the Text View
+        tvShowAge.text = "Your Age is $userAgeInYears Years"
     }
 }
